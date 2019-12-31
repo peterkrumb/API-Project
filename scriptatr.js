2264c0f4aec8318f851292132ed087dee5202991
andrewtr218 <andrewtr218@gmail.com>
2019-12-30 18:21:29 -0600

    Updated links to CDNS for JQuery and materialize, tested and confirmed functionality for onclick

--- scriptatr.js
+++ scriptatr.js
@@ -1,12 +1,12 @@
-$("#enter").on("click", function (event) {
-    event.preventdefault();
+$(".enter").on("click", function (event) {
+    event.preventDefault();
     var player = $("#textarea1").val().trim();
     
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log("Got reponse:" + response);
-    });
+    console.log("https://developers.google.com/youtube/v3/docs/search/list?apix_params=%7B%22part%22%3A%22snippet%22%2C%22maxResults%22%3A1%2C%22q%22%3A%22Steph%20Curry%20Highlights%22%7D");
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log("Got reponse:" + response);
+//     });
 });
