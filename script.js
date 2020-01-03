ee3b7c289ad214c23b443152851b747ab9c517a7
peterkrumb <petekrumb@gmail.com>
2020-01-03 14:25:36 -0600

    added render player name giphy

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ $("#select-player").on("click", function(event) {
   $.ajax(settings).done(function (response) {
   console.log(response.data[0]);
   var playerID = (response.data[0].id);
+  var playerCity = (response.data[0].first_name + response.data[0].last_name);
+  console.log(playerCity);
   console.log(playerID);
+  
 
   var settings2 = {
     "url": "https://balldontlie.io/api/v1/season_averages?season=2019&player_ids[]=" + playerID,
@@ -19,17 +22,36 @@ $("#select-player").on("click", function(event) {
   };
   $.ajax(settings2).done(function (response2) {
     console.log(response2);
+    $(".GP").text(response2.data[0].games_played)
+    $(".MIN").text(response2.data[0].min)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
+    $(".GP").text(response2.data[0].games_played)
 
-  
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=rQLAu0nfEnZbuhYVAVBwo4O6Rv3Ydn1c&q="+playerCity+"&limit=1&offset=0&rating=G&lang=en"
 
-  //var playerFirst = $("<h1>").text(response.data[0].first_name);
-  //var playerLast = $("<h1>").text(response.data[0].last_name);
-  
-    //$("#player-vitals").append(playerFirst, playerLast);
-});
-  });
-
-  
-  
-
-});
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+      }).then(function (response) {
+          console.log(response.data[0].url);
+          console.log(response);
+        //   $(".video").text(response.data[0].url);
+          $("iframe").attr("src", response.data[0].embed_url);
+          $("#giphylink2").attr("href", response.data[0].url);
+      });
